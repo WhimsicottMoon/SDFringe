0caa5ee65315d5fea6f5c7ff3d40474d23d80a0f
WhimsicottMoon <whimsicottmoon@gmail.com>
2019-05-16 23:55:42 -0400

    Code from 2018 Fringe?

---
+++ FringePassesv2.java
@@ -0,0 +1,111 @@
+//Veronica Tang  05/27/18
+//trying to get the hashmap to work
+//got rid of all lowercase letters
+
+import java.util.*;
+import java.nio.file.*;
+import java.io.*;
+import java.lang.*;
+import java.nio.charset.*;
+
+public class FringePassesv2
+{
+  private static final char[] NUMSANDLETTERS = {'A', 'B', 'C', 'D', 'E' ,'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+  static final String[] passcodes = {"03","05","10", "CC","01", "AC", "VP"};
+  static final boolean testing = false;
+  
+  //TEST FONTS!  CAP "S" WORKS OR  NO?  Hopefully will work...??  
+  
+  public static void main(String[] args)
+  {
+    int numOfThree = 0;
+    int numOfFive = 0;
+    int numOfTen = 0;
+    int numOfComp = 0;
+    int numBomb = 0;
+    int numOfArtistComp = 0;
+    int numOfVIP = 20;
+    int[] tasks = {numOfThree, numOfFive, numOfTen, numOfComp, numBomb,numOfArtistComp, numOfVIP};
+    
+    //Create hashmap with pre-existing codes
+    //this part works
+    HashMap<String, String> codes = new HashMap<String, String>();
+    try
+    {
+      List<String> numbers = Files.readAllLines(Paths.get("C:\\Veronica\\Fringe\\2018\\AllCodes.txt"), StandardCharsets.UTF_8);
+      for(int i = 0 ; i< numbers.size(); i++)
+      {
+        codes.put(numbers.get(i), numbers.get(i));
+      }
+      System.out.println("here");
+      System.out.println(codes);
+    }
+    catch(Exception hi)
+    {
+      System.out.println("nope");
+    }
+    
+    try
+    {
+      File file = new File("BOCode.txt"); //most recent stuff, spaced in 15s for printing
+      // creates the file
+      file.createNewFile();
+      // creates a FileWriter Object
+      FileWriter writer = new FileWriter(file);
+      File programFile = new File("NewCodes.txt");
+      programFile.createNewFile();
+      FileWriter codeWriter = new FileWriter(programFile);
+      
+      int count = 0; //count = number of codes generated thus far (at any point in the next chunk of code)
+      int limit = 0;
+      for (int a: tasks)
+        limit+=a; // so limit becomes the total number of codes to generate; each index of task represents the number of codes to generate for a specific type of code       
+      //generate all the codes
+      for(int x = 0; x<tasks.length; x++)
+      {
+        for(int y = 0; y<tasks[x]; y++)
+        {
+          String passcode = generate(x, codes);
+          codes.put(passcode, passcode);
+          count++;
+          System.out.println(passcode);
+          // Writes the content to the files
+          writer.write(passcode+System.getProperty("line.separator"));
+          if((count)%15 == 0)
+            writer.write(System.getProperty("line.separator"));
+          //this next part is just so we don't get an awkward space at the end of the program.
+          if(count!=limit)
+            codeWriter.write(passcode+ System.getProperty("line.separator"));
+          else
+            codeWriter.write(passcode);
+        }          
+      }
+      writer.flush();
+      writer.close();
+      codeWriter.flush();
+      codeWriter.close();
+    }
+    catch(Exception hi)
+    {
+      System.out.println("Printing problem");
+    }
+    
+    System.out.println(codes);
+    System.out.println("Done");
+  }
+  public static String generate(int x, HashMap<String,String> codes)
+  {
+          String passcode = passcodes[x]; //passcodes gives us the prefix, originally
+          for(int i = 0; i<7; i++)
+          {
+            passcode = passcode + NUMSANDLETTERS[((int)(Math.random() * (NUMSANDLETTERS.length)))]; //append the random string stuffs
+          }
+          if(testing)
+          {
+            passcode = passcode + "TEST";
+            if(codes.containsKey(passcode))
+              passcode = generate(x, codes);
+          }
+      return passcode;
+  }
+}
---
+++ Webdriverv2.py
@@ -0,0 +1,124 @@
+#Veronica Tang, 06/14/18
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+import sys
+import time
+
+amountoff = "100"
+desc = " Show Pass"
+tagnum = [1, 3, 4, 5, 25, 44]#index starts a 1, stupid Ticketleap
+startdate = "06/15/18" #mm/dd/yy
+starttime = "12:00"
+startampm = "am"
+enddate = "07/03/18" #mm/dd/yy
+endtime = "12:00"
+endampm = "am"
+
+list = [line.rstrip('\n') for line in open('NewCodes.txt')]
+for code in list:
+    code = code[2:]
+
+def testcode(code):
+    if code[:2] == "03":
+        return "3"
+    elif code[:2] == "05":
+        return "5"
+    elif code[:2] == "10":
+        return "10"
+    else:
+        return "1"
+class Testcode123(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://veronicatangfringe.ticketleap.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_code123(self):
+        driver = self.driver
+        driver.get("http://www.ticketleap.com/login/?next=http%3A//veronicatangfringe.ticketleap.com/")
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys("tangv@bishops.com")
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys("Moonstar5181")
+        driver.find_element_by_id("email_login_button").click()
+        time.sleep(1)
+        for code in list:
+            driver.get("about:home")
+            time.sleep(1)
+            driver.get("https://sdfringe18.ticketleap.com/admin/promote/discount-codes/#dialog=/admin/promote/discount-code/add")
+            driver.find_element_by_id("id_assigned_to_0").click()
+            time.sleep(1)
+            driver.find_element_by_id("id_code").clear()
+            driver.find_element_by_id("id_code").send_keys(code)
+            driver.find_element_by_id("id_description").clear()
+            driver.find_element_by_id("id_description").send_keys(testcode(code)+ desc)
+            #we never do amount off anymore, so this next part gets to go
+            #driver.find_element_by_id("id_type_1").click()
+            #driver.find_element_by_xpath("//table[@id='discount_code_type_toggle']/tbody/tr/td[2]/div/label[2]").click()
+            #driver.find_element_by_id("id_amount").clear()
+            driver.find_element_by_id("id_amount").send_keys("100")
+            #start date
+            driver.find_element_by_id("id_start_date").send_keys(startdate)
+            driver.find_element_by_id("id_start_date").click()
+            driver.find_element_by_id("id_start_time").clear()
+            driver.find_element_by_id("id_start_time").send_keys(starttime)
+            Select(driver.find_element_by_id("id_start_ampm")).select_by_visible_text(startampm)
+            #end date
+            driver.find_element_by_id("id_end_date").send_keys(enddate)
+            driver.find_element_by_id("id_end_date").click()
+            driver.find_element_by_id("id_end_time").clear()
+            driver.find_element_by_id("id_end_time").send_keys(endtime)
+            Select(driver.find_element_by_id("id_end_ampm")).select_by_visible_text(endampm)
+            #excluding certain ones
+            Select(driver.find_element_by_id("id_scope")).select_by_visible_text("Specific Events")
+            driver.find_element_by_css_selector("div.tl-fs-select").click()
+            driver.find_element_by_css_selector("span.tl-fs-dd-header-label").click()
+            for x in range (0, len(tagnum)):
+                driver.find_element_by_xpath("//*[@id='undefined-dropdown']/div/ul[2]/li["+str(tagnum[x])+"]/label/input").click()
+                #print(tagnum[x])
+            driver.find_element_by_css_selector("button.tl-fs-dd-done.secondary-button").click()
+            if(testcode(code)!="Unlimited"):
+                #selecting limit uses
+                driver.find_element_by_id("id_limit_uses_2").click()
+                driver.find_element_by_xpath("//form[@id='discount-code-form']/div[2]/table[11]/tbody/tr/td[2]/div/label[3]").click()
+                #max uses
+                driver.find_element_by_id("id_maximum_ticket_uses").clear()
+                driver.find_element_by_id("id_maximum_ticket_uses").send_keys(testcode(code))
+            driver.find_element_by_name("submit").click()
+            print(code)
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
